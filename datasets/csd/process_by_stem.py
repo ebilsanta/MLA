f926f7db247dd5a39e5e72e9b7a04c7d9111ad24
Thaddeus <thaddeusleezx@gmail.com>
2023-10-24 22:24:43 +0800

    update process_by_stem
    delete stems used for test to prevent showing up in train
    create mixtures for train and test

--- datasets/csd/process_by_stem.py
+++ datasets/csd/process_by_stem.py
@@ -31,13 +31,17 @@ def mono_to_stereo(input_file, output_file):
 def generate_test(input_dir, output_dir):
     source_folders = ['alto', 'tenor', 'bass', 'soprano']
     test_folder = os.path.join(output_dir, "test")
+    mixture_folder = os.path.join(test_folder, "mixture")
+    mix_no = 1
     os.makedirs(test_folder, exist_ok=True)
+    os.makedirs(mixture_folder, exist_ok=True)
     for song_folder in os.listdir(input_dir):
         song_path = os.path.join(input_dir, song_folder)
 
         if not os.path.isdir(song_path):
             continue
 
+        combined_audio = AudioSegment.silent(duration=0) 
         for source_folder in source_folders:
             source_folder_path = os.path.join(song_path, source_folder)
             voice_files = os.listdir(source_folder_path)
@@ -50,13 +54,29 @@ def generate_test(input_dir, output_dir):
                 voice_type_folder = os.path.join(test_folder, source_folder)
                 os.makedirs(voice_type_folder, exist_ok=True)
 
+                source_audio = AudioSegment.from_file(source_file_path)
+
+                # Use the parameters of the first audio file for the combined file
+                if source_folder == source_folders[0]:
+                    combined_audio = source_audio
+
+                else:
+                    combined_audio = combined_audio.overlay(source_audio)
+
                 # Copy the source track to the voice type folder
                 shutil.copy2(source_file_path, os.path.join(voice_type_folder, source_file))
+                os.remove(source_file_path)
+
+        combined_audio.export(os.path.join(mixture_folder, f"mixture{mix_no}.wav"), format="wav")
+        mix_no += 1
 
 
 def generate_train(input_dir, output_dir):
     source_folders = ['alto', 'tenor', 'bass', 'soprano']
     train_folder = os.path.join(output_dir, "train")
+    mixture_folder = os.path.join(train_folder, "mixture")
+    os.makedirs(mixture_folder, exist_ok=True)
+    mix_no = 1
     for song_folder in os.listdir(input_dir):
         song_path = os.path.join(input_dir, song_folder)
 
@@ -68,13 +88,25 @@ def generate_train(input_dir, output_dir):
 
         # Create a new folder for each voice type in the train folder
         for idx, combination in enumerate(combinations, start=1):
+            combined_audio = AudioSegment.silent(duration=0)
             for source_folder, source_file in zip(source_folders, combination):
                 source_file_path = os.path.join(song_path, source_folder, source_file)
+                source_audio = AudioSegment.from_file(source_file_path)
+
+                # Use the parameters of the first audio file for the combined file
+                if source_folder == source_folders[0]:
+                    combined_audio = source_audio
+
+                else:
+                    combined_audio = combined_audio.overlay(source_audio)
 
                 # Copy individual .wav files
                 voice_type_folder = os.path.join(train_folder, source_folder)
                 os.makedirs(voice_type_folder, exist_ok=True)
                 shutil.copy2(source_file_path, os.path.join(voice_type_folder, source_file))
+        
+            combined_audio.export(os.path.join(mixture_folder, f"mixture{mix_no}.wav"), format="wav")
+            mix_no += 1
 
 
 def preprocess_by_stem(input_dir, output_dir):
